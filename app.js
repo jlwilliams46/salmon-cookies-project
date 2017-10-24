26501784d0127571ddbe21477d076737532cdafd
Jamie Williams <32447139+jlwilliams46@users.noreply.github.com>
2017-10-23 22:46:03 -0700

    completed object literal and function

---
+++ app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+//1st and Pike
+var firstandPike = {
+  maxCustomers: 23,
+  minCustomers: 65,
+  avgCookies: 6.3,
+};
+
+//SeaTac Airport
+var seatacAirport = {
+  maxCustomers: 3,
+  minCustomers: 24,
+  avgCookies: 1.2,
+};
+
+//Seattle Center
+var seattleCenter = {
+  maxCustomers: 11,
+  minCustomers: 38,
+  avgCookies: 3.7,
+};
+
+//Capitol Hill
+var capitolHill = {
+  maxCustomers: 20,
+  minCustomers: 38,
+  avgCookies: 2.3,
+};
+
+//Alki
+var alki = {
+  maxCustomers: 2,
+  minCustomers: 16,
+  avgCookies: 4.6,
+};
+
+function randNum() {
+  return Math.random();
+}
